b0f2f0a1067e16f5536bd6088d5ddaf6be32919c
Anirudh Gupta <anirudhgupta281998@gmail.com>
2020-03-20 17:55:27 +0530

    commented unsupported languages

--- GoogleTranslate/gtranslate.py
+++ GoogleTranslate/gtranslate.py
@@ -239,8 +239,20 @@ OUTPUTLANGUAGE=sys.argv[2]
 INFILE=sys.argv[3]
 
 if OUTPUTLANGUAGE == "all":
-    LANGS = ["af","ak","sq","am","ar","hy","az","eu","be","bem","bn","bh","bs","br","bg","km","ca","chr","ny","co","hr","cs","da","nl","en","eo","et","ee","fo","tl","fi","fr","fy","gaa","gl","ka","de","el","gn","gu","ht","ha","haw","iw","hi","hu","is","ig","id","ia","ga","it","ja","jw","kn","kk","rw","rn","kg","ko","kri","ku","ckb","ky","lo","la","lv","ln","lt","loz","lg","ach","mk","mg","ms","ml","mt","mi","mr","mfe","mo","mn","sr-ME","ne","pcm","nso","no","nn","oc","or","om","ps","fa","pl","pt-BR","pt-PT","pa","qu","ro","rm","nyn","ru","gd","sr","sh","st","tn","crs","sn","sd","si","sk","sl","so","es","su","sw","sv","tg","ta","tt","te","th","ti","to","lua","tum","tr","tk","tw","ug","uk","ur","uz","vi","cy","wo","xh","yi","yo","zu"]
-    LANGS = ["te","th","ti","to","lua","tum","tr","tk","tw","ug","uk","ur","uz","vi","cy","wo","xh","yi","yo","zu"]
+    LANGS = ["af","ak","sq","am","ar","hy","az","eu","be","bem","bn","bh","bs","br",
+    "bg","km","ca","chr","ny","co","hr","cs","da","nl","en","eo","et","ee","fo","tl",
+    "fi","fr","fy","gaa","gl","ka","de","el","gn","gu","ht","ha","haw","iw","hi","hu",
+    "is","ig","id","ia","ga","it","ja","jw","kn","kk","rw","rn","kg","ko","kri","ku",
+    "ckb","ky","lo","la","lv","ln","lt","loz","lg","ach","mk","mg","ms","ml","mt",
+    "mi","mr","mfe","mo","mn",
+    # "sr-ME",
+    "ne","pcm","nso","no","nn","oc","or","om","ps","fa","pl",
+    # "pt-BR","pt-PT",
+    "pa","qu","ro","rm","nyn","ru","gd","sr",
+    # "sh",
+    "st","tn","crs","sn","sd","si","sk","sl","so","es","su","sw","sv","tg","ta",
+    "tt","te","th","ti","to","lua","tum","tr","tk","tw","ug","uk","ur","uz","vi",
+    "cy","wo","xh","yi","yo","zu"]
 else:
     LANGS = [OUTPUTLANGUAGE]
 
